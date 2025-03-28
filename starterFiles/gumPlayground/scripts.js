d9c465ed895f62bc0f8762589df09de7026f0d97
Aruminiya <leo80103@gmail.com>
2025-03-28 10:15:03 +0800

    feat: So far, Chapter 9

---
+++ starterFiles/gumPlayground/scripts.js
@@ -0,0 +1,47 @@
+const videoEl = document.querySelector('#my-video');
+let stream = null; // Initialize stream to null
+
+const constraints = {
+  audio: true, // user's microphone, if available
+  video: true
+};
+
+const getMicAndCamera = async () => {
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    console.log('Got MediaStream:', stream);
+    changeButtons([
+      'green', 'blue', 'blue', 'grey', 'grey', 'grey', 'grey', 'grey'
+    ]);
+  }
+  catch (err) {
+    console.error(err);
+  }
+};
+
+const showMyFeed = () => {
+  console.log('showMyFeed is working');
+  if (!stream) {
+    alert('Stream still loading...');
+    return;
+  }
+  videoEl.srcObject = stream;
+  const tracks = stream.getTracks();
+  console.log('Got MediaStreamTracks:', tracks);
+  changeButtons([
+    'green', 'green', 'blue', 'blue', 'blue', 'grey', 'grey', 'blue'
+  ]);
+};
+
+const stopMyFeed = () => {
+  const tracks = stream.getTracks();
+  tracks.forEach(track => track.stop());
+  stream = null;
+  changeButtons([
+    'blue', 'grey', 'grey', 'grey', 'grey', 'grey', 'grey', 'grey'
+  ]);
+};
+
+document.querySelector('#share').addEventListener('click', getMicAndCamera);
+document.querySelector('#show-video').addEventListener('click', showMyFeed);
+document.querySelector('#stop-video').addEventListener('click', stopMyFeed);
