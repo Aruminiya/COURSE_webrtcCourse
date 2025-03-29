4582eb39af645eedf2afdda75588daff6e0d145d
Aruminiya <leo80103@gmail.com>
2025-03-29 14:37:28 +0800

    feat: Chapter 16. Loading up input/output options

--- starterFiles/gumPlayground/inputOutput.js
+++ starterFiles/gumPlayground/inputOutput.js
@@ -1,8 +1,25 @@
+const audioInputEl = document.querySelector('#audio-input');
+const audioOutputEl = document.querySelector('#audio-output');
+const videoInputEl = document.querySelector('#video-input');
+
 const getDevices = async() => {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
-    console.log(devices);
-    return devices;
+    console.log("媒體設備", devices);
+    devices.forEach(device => {
+      const option = document.createElement('option');
+      option.value = device.deviceId;
+      option.innerText = device.label;
+      if (device.kind === 'audioinput') {
+        audioInputEl.appendChild(option);
+      }
+      else if (device.kind === 'audiooutput') {
+        audioOutputEl.appendChild(option);
+      }
+      else if (device.kind === 'videoinput') {
+        videoInputEl.appendChild(option);
+      }
+    });
   }
   catch(err) {
     console.log(err);
@@ -11,15 +28,56 @@ const getDevices = async() => {
 };
 
 const changeAudioInput = async(e) => {
-  console.log('changeAudioInput');
+  const deviceId = e.target.value;
+  const newConstraints = {
+    audio: {
+      deviceId: { exact: deviceId }
+    },
+    video: true 
+  }; 
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(newConstraints);
+    const tracks = stream.getAudioTracks();
+    console.log('tracks', tracks);
+  }
+  catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
 };
 
 const changeAudioOutput = async(e) => {
-  console.log('changeAudioOutput');
+  try {
+    /*
+      的主要功能是更改音頻元素的輸出設備。
+      例如，如果你有多個音頻輸出設備（如內建揚聲器和外接耳機），
+      你可以使用 setSinkId 將音頻輸出到指定的設備。
+    */
+    await videoEl.setSinkId(e.target.value);
+    console.log('Output device set to ' + e.target.value);
+  } catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
 };
 
-const changeVideoInput = async() => {
-  console.log('changeVideoInput');
+const changeVideoInput = async(e) => {
+  const deviceId = e.target.value;
+  const newConstraints = {
+    audio: true,
+    video: {
+      deviceId: { exact: deviceId }
+    }, 
+  }; 
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(newConstraints);
+    const tracks = stream.getVideoTracks();
+    console.log('tracks', tracks);
+  }
+  catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
 };
 
 getDevices();
